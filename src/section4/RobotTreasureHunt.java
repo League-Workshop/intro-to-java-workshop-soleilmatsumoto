38ad564eb63a17bd1f025a1f91a743433a02a2b6
League Student <league@AdministorsiMac.attlocal.net>
2019-06-06 14:20:21 -0700

    day 4

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,43 +16,51 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot robot= new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		robot.setAngle(0);
+		robot.microMove(5);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		robot.setAngle(180);
+		robot.microMove(5);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+robot.setAngle(270);
+robot.microMove(5);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+	robot.setAngle(90);
+	robot.microMove(5);
+
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = robot.getX();
+		int robotYLocation = robot.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println("X= "+robotXLocation+" Y= "+robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
+		JOptionPane.showMessageDialog(null, "go to the bony thing");
 		
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 		
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
+		if(robotXLocation==261&&robotYLocation==129) {
+				treasureFound();}
 		
 	}
 
